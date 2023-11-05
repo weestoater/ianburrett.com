dc1d31ae134c43c0825619727ddfc34323c40fd8
Ian Burrett <ian@weestoater.com>
2023-11-05 16:13:53 +0000

    Added second page and header

--- src/app.js
+++ src/app.js
@@ -1,20 +1,23 @@
 import React from "react";
+import { HashRouter, Routes, Route } from "react-router-dom";
 import "./sass/styles.scss";
 
-import burtImg from "./assets/img/burt.png";
+//--    pages
+import Home from "./pages/home";
+import About from "./pages/about";
+
+//--    components
+import Header from "./components/header";
 
 const App = () => {
-  document.title = "Home - ianburrett.com";
-
   return (
-    <div className="homepage">
-      <h1>Welcome</h1>
-      <img src={burtImg} alt="Avatar of Ian Burrett" className="avatar" />
-      <p className="lead">
-        <i className="bi bi-gear text-success me-2"></i> This is a small testing
-        space for web ideas by Ian Burrett.
-      </p>
-    </div>
+    <HashRouter>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+    </HashRouter>
   );
 };
 
---
+++ src/pages/about.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const About = () => {
+  document.title = "About - ianburrett.com";
+
+  return (
+    <>
+      <h1>About</h1>
+    </>
+  );
+};
+
+export default About;
